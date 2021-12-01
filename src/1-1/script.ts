9c13ccdf6f3cd973a293b00eab4bf731d0aaf7c4
Stefan Niewerth <stefan.niewerth@gmx.net>
2021-12-01 19:58:59 +0100

    Day 1-1

---
+++ src/1-1/script.ts
@@ -0,0 +1,19 @@
+import fs from 'fs';
+
+const text = fs.readFileSync(`${__dirname}/input.txt`).toString();
+const textByLine = text.split('\n');
+const numbersByLine = textByLine.map((text) => parseInt(text));
+
+const increases = numbersByLine.reduce((previousValue, currentValue, currentIndex, array) => {
+	if (currentIndex === 0) {
+		return previousValue;
+	}
+
+	if (currentValue > array[currentIndex - 1]!) {
+		return previousValue + 1;
+	}
+
+	return previousValue;
+}, 0);
+
+console.log(increases);
