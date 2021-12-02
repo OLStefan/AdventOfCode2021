591fa6f074b33f7e2c2ffdee58d380781a6dab04
Stefan Niewerth <stefan.niewerth@gmx.net>
2021-12-02 15:10:46 +0100

    day 2

---
+++ src/day-2/puzzle-1.ts
@@ -0,0 +1,23 @@
+import fs from 'fs';
+
+const text = fs.readFileSync(`${__dirname}/input.txt`).toString();
+const textByLine = text.split('\n');
+const commandsByLine = textByLine.map((text) => text.split(' ') as [string, string]);
+
+const increases = commandsByLine.reduce(
+	(previousValue, currentValue) => {
+		const [command, number] = currentValue;
+		const numberParsed = parseInt(number);
+		switch (command) {
+			case 'up':
+				return { ...previousValue, depth: previousValue.depth - numberParsed };
+			case 'down':
+				return { ...previousValue, depth: previousValue.depth + numberParsed };
+			default:
+				return { ...previousValue, horizontal: previousValue.horizontal + numberParsed };
+		}
+	},
+	{ depth: 0, horizontal: 0 },
+);
+
+console.log(increases.depth * increases.horizontal);
---
+++ src/day-2/puzzle-2.ts
@@ -0,0 +1,29 @@
+import fs from 'fs';
+
+const text = fs.readFileSync(`${__dirname}/input.txt`).toString();
+const textByLine = text.split('\n');
+const commandsByLine = textByLine.map((text) => text.split(' ') as [string, string]);
+
+const increases = commandsByLine.reduce(
+	(previousValue, currentValue) => {
+		const [command, number] = currentValue;
+		const numberParsed = parseInt(number);
+		switch (command) {
+			case 'up':
+				return { ...previousValue, aim: previousValue.aim - numberParsed };
+			case 'down':
+				return { ...previousValue, aim: previousValue.aim + numberParsed };
+			default:
+				return {
+					...previousValue,
+					horizontal: previousValue.horizontal + numberParsed,
+					depth: previousValue.depth + previousValue.aim * numberParsed,
+				};
+		}
+	},
+	{ depth: 0, horizontal: 0, aim: 0 },
+);
+
+console.log(increases);
+
+console.log(increases.depth * increases.horizontal);
