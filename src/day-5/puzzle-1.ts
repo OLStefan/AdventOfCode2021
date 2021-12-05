53b1305f062d7a0e0c42e692f95ae64af528f3b8
Stefan Niewerth <stefan.niewerth@gmx.net>
2021-12-05 11:16:46 +0100

    day 5-1

---
+++ src/day-5/puzzle-1.ts
@@ -0,0 +1,55 @@
+import fs from 'fs';
+import { cloneDeep } from 'lodash';
+
+const text = fs.readFileSync(`${__dirname}/input.txt`).toString().trim();
+const textByLine = text.split('\n');
+
+function readVents(lines: string[]): Array<[[number, number], [number, number]]> {
+	return lines.map((line) => {
+		const points = line.split(' -> ') as [`${number},${number}`, `${number},${number}`];
+		const parsedPoints = points.map((point) => point.split(',').map((coord) => parseInt(coord)));
+		return parsedPoints as [[number, number], [number, number]];
+	});
+}
+
+function mapVents(vents: ReturnType<typeof readVents>) {
+	const maxSize = vents.reduce(
+		(prev, curr) => {
+			return {
+				x: Math.max(prev.x, curr[0][0] + 1, curr[1][0] + 1),
+				y: Math.max(prev.y, curr[0][1] + 1, curr[1][1] + 1),
+			};
+		},
+		{ x: 10, y: 10 },
+	);
+
+	const ventMap: number[][] = Array(maxSize.y)
+		.fill(undefined)
+		.map(() => Array(maxSize.x).fill(0));
+
+	return vents.reduce((prev, curr) => {
+		const next = cloneDeep(prev);
+		if (curr[0][0] === curr[1][0]) {
+			const x = curr[0][0];
+			for (let i = Math.min(curr[0][1], curr[1][1]); i <= Math.max(curr[0][1], curr[1][1]); i++) {
+				next[i]![x]++;
+			}
+		} else if (curr[0][1] === curr[1][1]) {
+			const y = curr[0][1];
+			for (let i = Math.min(curr[0][0], curr[1][0]); i <= Math.max(curr[0][0], curr[1][0]); i++) {
+				next[y]![i]++;
+			}
+		}
+		return next;
+	}, ventMap);
+}
+
+function countTwos(ventMap: number[][]) {
+	return ventMap.reduce((lineSum, line) => {
+		return lineSum + line.filter((entry) => entry >= 2).length;
+	}, 0);
+}
+
+const points = readVents(textByLine);
+const ventMap = mapVents(points);
+console.log(countTwos(ventMap));
